f61946c35bb6b14b157ae7834d295cec15f568d9
berrrchik <berrrchik@mail.ru>
2025-08-27 23:20:55 +0300

    Initial Commit

---
+++ CryptoCurrency/CryptoCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyApp.swift
+//  CryptoCurrency
+//
+//  Created by Анастасия Берчик on 8/27/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
