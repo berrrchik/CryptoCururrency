101afd4250f0169fb79e74f26cd453807cdca739
berrrchik <berrrchik@mail.ru>
2025-09-02 21:10:37 +0300

    feat: add navigation to HomeView

--- CryptoCurrency/CryptoCurrencyApp.swift
+++ CryptoCurrency/CryptoCurrencyApp.swift
@@ -1,17 +1,13 @@
-//
-//  CryptoCurrencyApp.swift
-//  CryptoCurrency
-//
-//  Created by Анастасия Берчик on 8/27/25.
-//
-
 import SwiftUI
 
 @main
 struct CryptoCurrencyApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                HomeView()
+                    .navigationBarHidden(true)
+            }
         }
     }
 }
