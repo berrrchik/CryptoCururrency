ef8f20cc0bbce947079a1c9f58fd5db163d5bb84
berrrchik <berrrchik@mail.ru>
2025-09-02 21:42:11 +0300

    feat(home): add dynamic button and animation for navigation bar

--- CryptoCurrency/Core/Home/Views/HomeView.swift
+++ CryptoCurrency/Core/Home/Views/HomeView.swift
@@ -1,20 +1,16 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
     var body: some View {
         ZStack {
             Color.theme.background
                 .ignoresSafeArea()
             
             VStack {
-                HStack {
-                    CircleButtonView(iconName: "info")
-                    Spacer()
-                    Text("Header")
-                    Spacer()
-                    CircleButtonView(iconName: "chevron.right")
-                }
-                .padding(.horizontal)
+                homeHeader
                 Spacer(minLength: 0)
             }
         }
@@ -25,5 +21,33 @@ struct HomeView: View {
     NavigationView {
         HomeView()
             .navigationBarHidden(true)
+    }
+}
+
+extension HomeView {
+    
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .animation(.none)
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                )
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundStyle(Color.theme.accent)
+                .animation(.none)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
     }
 }
