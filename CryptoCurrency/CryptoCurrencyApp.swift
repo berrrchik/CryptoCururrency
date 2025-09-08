5ff46f44ea29309ca952571835f876e8e0a12456
berrrchik <berrrchik@mail.ru>
2025-09-08 21:49:58 +0300

    feat: add home view model as environment object for app

--- CryptoCurrency/CryptoCurrencyApp.swift
+++ CryptoCurrency/CryptoCurrencyApp.swift
@@ -2,12 +2,16 @@ import SwiftUI
 
 @main
 struct CryptoCurrencyApp: App {
+    
+    @StateObject private var vm = HomeViewModel()
+    
     var body: some Scene {
         WindowGroup {
             NavigationView {
                 HomeView()
                     .navigationBarHidden(true)
             }
+            .environmentObject(vm)
         }
     }
 }
